3c3438a761b1976d9c9b0d2c8cb005363a38dcad
hilaryperillo <hilary.perillo@escola.pr.gov.br>
2026-09-03 12:29:54 -0300

    sav

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-Seleciona todos os elementos <button> da página e os guarda na constante 'botoes' como uma lista
+// Seleciona todos os elementos <button> da página e os guarda na constante 'botoes' como uma lista
 const botoes = document.querySelectorAll("button");
 
 // Passa por cada botão da lista (um por um) para aplicar as regras abaixo
 botoes.forEach(function (botao) {
 
-    // Cria uma variável interna para controlar se o BOTÃO ATUAL foi clicado ou não (começa como falso)
+    // Cria uma variável interna para controlar se O BOTÃO ATUAL foi clicado ou não (começa como falso)
     let curtiu = false;
 
     // Adiciona um "escutador" que aguarda o clique especificamente neste botão
@@ -21,13 +21,15 @@ botoes.forEach(function (botao) {
         // Se o usuário ainda não tiver clicado neste botão (curtiu é igual a false)
         if (curtiu === false) {
             texto.textContent++; // Soma +1 ao número atual do contador
-            curtiu = true; // Muda o status para true (indicando que agora está "curtido")
-        }
+            curtiu = true;       // Muda o status para true (indicando que agora está "curtido")
 
         // Caso o usuário clique novamente (ou seja, curtiu já era true)
-        else {
+        } else {
             texto.textContent--; // Subtrai -1 do número do contador (desfaz a curtida)
-            curtiu = false; // Volta o status para false (permitindo curtir de novo depois)
+            curtiu = false;      // Volta o status para false (permitindo curtir de novo depois)
         }
     }
 });
+
+
+
